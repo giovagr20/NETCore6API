7cecebf89be594f310619ef9fc37d4918b1709bd
Giovanni Gomez Restrepo <giovannyg32@gmail.com>
2022-08-05 22:40:30 -0500

    Se agregan pruebas unitarias

---
+++ APIAnimeTest/ControllerTest/WeatherForecastControllerTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using APIAnime;
+using APIAnime.Controllers;
+
+namespace APIAnimeTest.ControllerTest
+{
+    public class WeatherForecastControllerTest
+    {
+        [SetUp]
+        public void SetUp()
+        {
+
+        }
+
+        [Test]
+        public void MustDeliverWeatherToday()
+        {
+           
+            var weather = new WeatherForecastController();
+
+            IEnumerable<WeatherForecast> weatherForecasts = weather.Get();
+
+            Assert.That(5, Is.EqualTo(weatherForecasts.Count()));
+
+        }
+    }
+}
+
